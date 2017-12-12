349d8c1bbe33e243e0c539c43c0a98ec51c2ff62
Quentin Monmert <qmonmert@MacBook-Pro-de-Cedric.local>
2017-12-12 14:57:19 +0100

    Success test

--- src/App.test.js
+++ src/App.test.js
@@ -5,6 +5,4 @@ import App from './App';
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
-  // Failed test
-  expect('test').toEqual('failed');
 });
